fdef6b7e9102274c7717e0359bf8707852fad86c
Mariana Paola Caballero Cabrera A01376544 <A01376544@itesm.mx>
2021-10-06 20:02:30 -0500

    commit videojuegos

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const express = require ("express")
 const path = require("path")
 const vjRoutes = require('./routes/videojuegos')
+const conRoutes = require('./routes/consola')
 const sequelize = require('./utils/database')
 const app = express()
+
+
+//Configurar el servidor y que sepa que es un Json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use( "/videojuegos",vjRoutes);
+//app.use( "/consola",vjRoutes);
 
 app.get("/prueba", (req, res) =>{
     res.send("Prueba del servidor funcianando")
 
 })
 
-app.listen(8084,()=>{
+/*app.listen(8084,()=>{
     console.log("Servidor en linea puerto 8084")
-})
+})*/
+
+//Que la aplicación escuche peticiones
+sequelize.sync()
+    .then(()=>{
+        app.listen(8080,()=>{
+            console.log("Aplicación web en línea en el puerto 8080")
+    })
+    })
+    .catch(err=>console.log(err))
 
 
-//Middleware
-app.use(express.static(path.join(__dirname,'public')))
 
-//Configurar el servidor y que sepa que es un Json
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine' , 'ejs')
 
-app.use( "/videojuegos",vjRoutes);
+
 
 
